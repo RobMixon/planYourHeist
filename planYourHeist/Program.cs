d4ed7d65451fbed88809f48922e32ff8111fa04e
Rob Mixon <mixonroberth@gmail.com>
2020-08-28 12:01:53 -0500

    phase 1 complete

--- planYourHeist/Program.cs
+++ planYourHeist/Program.cs
@@ -1,9 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace planYourHeist {
+namespace PlanYourHeist {
     class Program {
         static void Main (string [] args) {
-            Console.WriteLine ("Hello World!");
+            Console.WriteLine ("Plan Your Heist!");
+            Console.WriteLine ("Enter team name");
+            string teamName = Console.ReadLine ();
+            Console.WriteLine ("Enter team member's names");
+            string name = Console.ReadLine ();
+            Console.WriteLine ("Enter team member's skill level");
+            string skillLevel = Console.ReadLine ();
+            Console.WriteLine ("Enter team member's courage factor");
+            string courageFactor = Console.ReadLine ();
+
+            Team Cheddar = new Team (teamName);
+            TeamMember Faith = new TeamMember (name, skillLevel, courageFactor);
+            Cheddar.addTeamMember (Faith);
+            // calling them with cheddar.ListTeamMembers
+            Cheddar.ListTeamMembers ();
+
         }
     }
 }
